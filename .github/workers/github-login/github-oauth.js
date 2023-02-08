2afff61f4c8689fd584b834a1d9847973bd9751e
Jeff Lindsay <progrium@gmail.com>
2023-02-08 10:51:59 -0600

    github: add github login worker and deploy action

---
+++ .github/workers/github-login/github-oauth.js
@@ -0,0 +1,63 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handle(event.request));
+});
+
+// use secrets
+const client_id = CLIENT_ID;
+const client_secret = CLIENT_SECRET;
+
+async function handle(request) {
+  // handle CORS pre-flight request
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
+  // redirect GET requests to the OAuth login page on github.com
+  if (request.method === "GET") {
+    return Response.redirect(
+      `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=${new URL(request.url).searchParams.get("scope")}`,
+      302
+    );
+  }
+
+  try {
+    const { code } = await request.json();
+
+    const response = await fetch(
+      "https://github.com/login/oauth/access_token",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "user-agent": "cloudflare-worker-github-oauth-login-demo",
+          accept: "application/json",
+        },
+        body: JSON.stringify({ client_id, client_secret, code }),
+      }
+    );
+    const result = await response.json();
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+    };
+
+    if (result.error) {
+      return new Response(JSON.stringify(result), { status: 401, headers });
+    }
+
+    return new Response(JSON.stringify({ token: result.access_token }), {
+      status: 201,
+      headers,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message, {
+      status: 500,
+    });
+  }
+}
