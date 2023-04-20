499f1a146c3a5fe83fb0fd50b0eb9fa11bc44323
taramk <taramkirkland@gmail.com>
2023-04-20 11:58:33 -0700

    move panel and outline styles to css

--- lib/ui/outline.tsx
+++ lib/ui/outline.tsx
@@ -128,59 +128,30 @@ export const OutlineNode: m.Component<Attrs, State> = {
     }
     return (
       <div onmouseover={hover} onmouseout={unhover}>
-        <div style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "start",
-          marginTop: "0.125rem",
-          marginBottom: "0.125rem"
-        }} >
-          <svg xmlns="http://www.w3.org/2000/svg"
-              style={{
-                cursor: "pointer",
-                flexShrink: "0",
-                width: "1rem", 
-                height: "1rem", 
-                position: "absolute", 
-                marginLeft: "-1rem", 
-                userSelect: "none",
-                marginTop: "0.25rem",
-                display: (state.hover)?"block":"none"
-              }}  
+        <div class="node-row-outer-wrapper flex flex-row items-start">
+          <svg class="node-menu-icon no-shrink" xmlns="http://www.w3.org/2000/svg"
+              style={{display: (state.hover)?"block":"none"}}  
               onclick={(e) => workbench.showMenu(e, {node, panel})}
               oncontextmenu={(e) => workbench.showMenu(e, {node, panel})} 
               data-menu="node"
-              fill="lightgray" 
               viewBox="0 0 16 16">
             <path style={{transform: "translateY(-1px)"}} fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
           </svg>
-          <svg onclick={toggle} ondblclick={open} oncontextmenu={(e) => workbench.showMenu(e, {node, panel})} data-menu="node" style={{
-              cursor: "pointer", 
-              flexShrink: "0", 
-              width: "1rem", 
-              height: "1rem", 
-              marginRight: "0.5rem", 
-              paddingLeft: "1px",
-              marginTop: "0.25rem"
-            }} xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 16 16">
-            {(node.childCount > 0 && !expanded)?<circle cx="8" cy="7" r="7" fill="lightgray" />:null}
+          <svg class="node-bullet no-shrink" onclick={toggle} ondblclick={open} oncontextmenu={(e) => workbench.showMenu(e, {node, panel})} data-menu="node" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            {(node.childCount > 0 && !expanded)?<circle id="node-collapsed" cx="8" cy="7" r="7" />:null}
             <circle cx="8" cy="7" r="3"/>
             {(node.refTo !== null)?<circle cx="8" cy="7" r="7" fill="none" stroke="gray" stroke-width="1" stroke-dasharray="3,3" />:null}
           </svg>
-          <div style={{flexGrow: "1", display: "flex", alignItems: "start"}}>
+          <div class="flex grow items-start">
             {(node.hasComponent(Checkbox)) ? <input type="checkbox" style={{marginTop: "0.3rem", marginRight: "0.5rem"}} onclick={toggleCheckbox} checked={node.getComponent(Checkbox).checked} />:null}
             <NodeEditor workbench={workbench} panel={panel} node={node} onkeydown={checkCommands} />
           </div>
         </div>
         {(expanded === true) &&
-          <div style={{
-            display: "flex",
-            flexDirection: "row",
-            paddingBottom: "0.25rem"
-          }}>
-            <div style={{width: "var(--8)", display: "flex"}} onclick={toggle}>
+          <div class="expanded-node flex flex-row">
+            <div class="indent flex" onclick={toggle}>
             </div>
-            <div style={{flexGrow: "1"}}>
+            <div class="grow">
               {(node.childCount > 0)
                 ?node.children.map(n => <OutlineNode key={n.id} workbench={workbench} panel={panel} node={n} />)
                 :<NewNode workbench={workbench} panel={panel} node={node} />
@@ -209,27 +180,18 @@ export const NewNode = {
       }
     }
     return (
-      <div style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: "0.125rem",
-        marginBottom: "0.125rem"
-      }} >
-        <svg style={{flexShrink: "0", width: "1rem", height: "1rem", marginRight: "0.5rem", paddingLeft: "1px"}} xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 16 16">
-          <circle cx="8" cy="7" r="7" fill="lightgray" />
-          <path fill="#555" style={{transform: "translate(0px, -1px)"}} d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+      <div class="new-node flex flex-row items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 16 16">
+          <circle cx="8" cy="7" r="7" />
+          <path style={{transform: "translate(0px, -1px)"}} d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
         </svg>
-        <div style={{flexGrow: "1", display: "flex"}}>
-        <input type="text"
-          oninput={startNew}
-          onkeydown={tabNew}
-          value={""}
-          style={{
-            border: "0px",
-            flexGrow: "1",
-            outline: "0px"
-          }} />
+        <div class="flex grow">
+          <input class="grow"
+            type="text"
+            oninput={startNew}
+            onkeydown={tabNew}
+            value={""}
+          />
         </div>
       </div>
     )
@@ -239,7 +201,7 @@ export const NewNode = {
 export const OutlineEditor: m.Component<Attrs> = {
   view ({attrs: {workbench, panel, node}, state}) {
     return (
-      <div style={{color: "var(--gray-900)"}}>
+      <div>
         {node.children.map(n => <OutlineNode key={n.id} workbench={workbench} panel={panel} node={n} />)}
         <NewNode workbench={workbench} panel={panel} node={node} />
       </div>
@@ -306,24 +268,10 @@ export const NodeEditor: m.Component = {
       }
     }
     
-    const style = {
-      outline: "none",
-      color: "inherit",
-      fontSize: "inherit",
-      fontWeight: "inherit",
-      fontFamily: "inherit",
-      padding: "0",
-      width: "100%",
-      boxSizing: "border-box",
-      resize: "none",
-      overflow: "hidden",
-      display: "block",
-      lineHeight: "1.45",
-      border: "none"
-    }
+    const style = {}
     return (
-      <div style={{width: "100%", marginBottom: "0.5rem"}}>
-        <textarea style={style} 
+      <div class="node-container">
+        <textarea
           id={`input-${panel.id}-${node.id}`}
           rows="1"
           onfocus={startEdit}
--- lib/ui/panel.tsx
+++ lib/ui/panel.tsx
@@ -28,47 +28,30 @@ export const Panel = {
       let newHeight = 20 + numberOfLineBreaks * 20;
       return newHeight;
     }
-    return <div class="panel flex flex-col grow" style={{
-        marginTop: "var(--padding)", 
-        marginLeft: "calc(var(--padding)*2)", 
-        marginRight: "calc(var(--padding)*2)", 
-        paddingBottom: "var(--padding)"}}>
-      <div class="bar" style={{
-        display: "flex", 
-        fontSize: "0.875rem",
-        color: "var(--gray-600)", 
-        gap: "var(--padding)", 
-        paddingLeft: "var(--padding)",
-        paddingRight: "var(--padding)",
-        marginBottom: "calc(var(--padding)*1.5)"}}>
+    return <div class="panel flex flex-col grow">
+      <div class="bar flex">
         {(panel.history.length>1)?
-          <div style={{rightPadding: "var(--padding)"}}>
+          <div class="panel-back" style={{rightPadding: "var(--padding)"}}>
             <svg onclick={goBack} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
             </svg>
           </div>
         :null}
-        <div style={{
-          flexGrow: "1"
-          }}>
+
+        <div class="panel-back-parent grow">
           {(node.parent && node.parent.id !== "@root") ? <span style={{cursor: "pointer"}} onclick={() => workbench.open(node.parent)}>{node.parent.name}</span> : <span>/</span>}
         </div>
+
         {(workbench.panels.flat().length>1)?
-          <div style={{display: "flex", gap: "var(--4)", zIndex: "0", color: "var(--gray-500)", alignItems: "center"}}>
-            <svg  onclick={maximize} style={{cursor: "pointer"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize-2"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>
+          <div class="panel-icons flex items-center">
+            <svg onclick={maximize} style={{cursor: "pointer"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize-2"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>
             <svg onclick={close} style={{cursor: "pointer"}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
           </div>
         :null}
       </div>
-      <div class="body flex flex-col" style={{
-        paddingLeft: "var(--padding)",
-        paddingRight: "var(--padding)"
-      }}>
-        <div oncontextmenu={(e) => workbench.showMenu(e, {node, panel})} data-menu="node" style={{
-            fontSize: "var(--6)",
-            color: "var(--gray-900)",
-            fontWeight: "700"
-          }}>
+
+      <div class="body flex flex-col">
+        <div class="title-node" oncontextmenu={(e) => workbench.showMenu(e, {node, panel})} data-menu="node">
           <NodeEditor workbench={workbench} panel={panel} node={node} disallowEmpty={true} />
         </div>
         {(node.hasComponent(Page)) ? 
