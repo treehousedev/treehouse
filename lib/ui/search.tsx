2e63cbe2b9ee93bed647cbfbc3fc94fda55758f6
taramk <taramkirkland@gmail.com>
2023-04-26 15:28:22 -0700

    initial mobile style updates (#113)

--- lib/ui/search.tsx
+++ lib/ui/search.tsx
@@ -107,10 +107,10 @@ export const Search: m.Component = {
 
 
     return (
-      <div class={(state.results.length > 0)?"search active":"search"} style={{position: "relative", display: "flex", flexGrow: "1"}}>
+      <div class={(state.results.length > 0)?"search active flex grow":"search flex grow"}>
         <div>
           <div class="flex" style={{margin: (state.results.length > 0)?"0":"1px"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search shrink-0"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
             <input type="text" placeholder="Search" value={state.query} onkeydown={onkeydown} oninput={autocomplete} style={{
               border: "0", 
               outline: "0", 
