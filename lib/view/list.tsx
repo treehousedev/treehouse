1f94bf14668b6cb8dbe1032cbad5de5c247dbdd0
Dwayne Harris <dwayne@dwayne.xyz>
2023-05-16 12:53:08 -0400

    view: use refTo node in list view if it exists

--- lib/view/list.tsx
+++ lib/view/list.tsx
@@ -3,16 +3,20 @@ import { OutlineNode } from "../ui/outline.tsx";
 
 export default {
   view({attrs: {workbench, path}}) {
+    let node = path.node;
+    if (path.node.refTo) {
+      node = path.node.refTo;
+    }
     return (
       <div class="list-view">
         <div class="fields">
-          {(path.node.getLinked("Fields").length > 0) &&
-            path.node.getLinked("Fields").map(n => <OutlineNode key={n.id} workbench={workbench} path={path.append(n)} />)
+          {(node.getLinked("Fields").length > 0) &&
+            node.getLinked("Fields").map(n => <OutlineNode key={n.id} workbench={workbench} path={path.append(n)} />)
           }
         </div>
         <div class="children">
-          {(path.node.childCount > 0)
-            ?path.node.children.map(n => <OutlineNode key={n.id} workbench={workbench} path={path.append(n)} />)
+          {(node.childCount > 0)
+            ?node.children.map(n => <OutlineNode key={n.id} workbench={workbench} path={path.append(n)} />)
             :<NewNode workbench={workbench} path={path} />
           }
         </div>
