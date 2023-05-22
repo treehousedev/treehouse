1f69f0cd39dc726bfee7106544685a865ce54150
taramk <taramkirkland@gmail.com>
2023-05-22 13:36:35 -0700

    Implement CSS Variables (#163)
    * just committing this so i can see my changes
    
    * finalize color variables
    
    * add font variable
    
    * update variables to use color as prefix
    
    * fix one last color prefix

--- lib/ui/node/new.tsx
+++ lib/ui/node/new.tsx
@@ -16,7 +16,7 @@ export const NewNode = {
     }
     return (
       <div class="new-node flex flex-row items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
           <circle cx="8" cy="7" r="7" />
           <path style={{transform: "translate(0px, -1px)"}} d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
         </svg>
