a8a389c3939d990ad172ec608bdc6daba2da9323
Dwayne Harris <dwayne@dwayne.xyz>
2023-05-23 19:12:22 -0400

    ui: added some placeholder handling and changed search node ui (#166)
    * ui: added some placeholder handling and changed search node ui
    
    * ui: focus search node on creation

--- lib/com/search.tsx
+++ lib/com/search.tsx
@@ -29,8 +29,12 @@ export class SearchNode {
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
   }
 
+  handlePlaceholder(): any {
+    return 'Enter search value';
+  }
+
   displayName(node: Node): any {
-    return `Search for "${node.name}"`;
+    return node.name;
   }
 
   onAttach(node: Node) {
