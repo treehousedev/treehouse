34574c5d74f91e756fea3952adc5be52e6c5bbbc
taramk <taramkirkland@gmail.com>
2023-07-03 11:42:06 -0700

    update tag colors to use theme variables (#231)

--- lib/com/tag.tsx
+++ lib/com/tag.tsx
@@ -111,7 +111,7 @@ const TagBadge = {
       }
     };
     return (
-      <div tabindex="1" class="badge flex flex-row items-center" onkeydown={onkeydown} style={{background: "gray", lineHeight: "var(--body-line-height)", paddingLeft: "0.25rem", paddingRight: "0.25rem", borderRadius: "4px", color: "white"}}>
+      <div tabindex="1" class="badge flex flex-row items-center" onkeydown={onkeydown} >
         <svg style={{width: "1rem", height: "1rem", marginRight: "0.25rem"}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path><line x1="7" y1="7" x2="7.01" y2="7"></line></svg>
         <div style={{whiteSpace: "nowrap"}}>{component.name}</div>
       </div>
