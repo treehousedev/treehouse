7e2c8b409efec30acf94b10fe879c17d2f89b22d
Jeff Lindsay <progrium@gmail.com>
2023-07-06 13:56:10 -0500

    web: 0.5.0 blog post

--- web/index.tsx
+++ web/index.tsx
@@ -73,7 +73,7 @@ export default (data) => (
 
   <div class="text-white text-center" style={{backgroundColor: "#1F842A"}}>
     <div class="mx-auto py-4">
-      <p style={{marginTop: "0px !important"}}>Treehouse is in early development. <a class="underline" href="/blog/v0.4.0">View release 0.4.0 &rarr;</a></p>
+      <p style={{marginTop: "0px !important"}}>Treehouse is in early development. <a class="underline" href="/blog/v0-5-0">View release 0.5.0 &rarr;</a></p>
     </div>
   </div>
 
