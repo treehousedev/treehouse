dda188180ade4ef1869534de726cc67bdd829fd2
Dwayne Harris <dwayne@dwayne.xyz>
2023-07-11 23:39:29 -0400

    lib: add styling to tabs view and make it actually work

--- lib/view/tabs.tsx
+++ lib/view/tabs.tsx
@@ -3,21 +3,27 @@ import { OutlineNode } from "../ui/outline.tsx";
 export default {
   view({ attrs: { workbench, path }, state }) {
     const node = path.node;
-    state.tabName = '';
-    state.fields = (state.fields === undefined) ? new Set() : state.fields;
+    state.tabs = (state.tabs === undefined) ? new Set() : state.tabs;
+    state.selectedTab = (state.selectedTab === undefined) ? "" : state.selectedTab;
     node.children.forEach(n => {
-      n.getLinked("Fields").forEach(f => state.fields.add(f.name));
+      state.tabs.add(n.raw);
+      if (state.selectedTab === "") state.selectedTab = n.raw.ID;
     });
+    const handleTabClick = (id) => {
+      state.selectedTab = id;
+    };
     return (
       <div class="tabs-view">
-        <ul class="tabs">
-          {[...state.fields].map(f => <li onclick={() => state.tabName = f}>{f}</li>)}
-        </ul>
+        <div class="tabs">
+          {[...state.tabs].map(n => <div class={n.ID === state.selectedTab ? "active" : ""} onclick={() => handleTabClick(n.ID)}>{n.Name}</div>)}
+          <div style={{ flexGrow: 1 }}></div>
+        </div>
         <div class="tab-content">
-          {node.children.filter(n => n.getLinked("Fields").filter(f => f.name === state.tabName)).map(n => (
+          {node.children.filter(n => n.id === state.selectedTab).map(n => (
             <OutlineNode key={n.id} workbench={workbench} path={path.append(n)} />
           ))}
         </div>
-        )
+      </div>
+    )
   }
 }
