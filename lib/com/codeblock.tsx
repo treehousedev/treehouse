55a7b8ce93daf53471a49469dcfc151cbffd5a98
Levon Ritter <lritter1@ottertune.com>
2024-11-17 17:21:34 -0500

    patch over some linter errors iwth codeblock

--- lib/com/codeblock.tsx
+++ lib/com/codeblock.tsx
@@ -1,11 +1,10 @@
 import { component } from "../model/components.ts";
-import { Node } from "../model/mod.ts";
-
+import { Workbench, Context } from "../workbench/mod.ts";
 
 export interface CodeExecutor {
   // executes the source and returns an output string.
   // exceptions in execution should be caught and returned as a string.
-  async execute(source: string, options: ExecuteOptions): string;
+  execute(source: string, options: ExecuteOptions): Promise<string>;
 
   canExecute(options: ExecuteOptions): boolean;
 }
@@ -16,7 +15,10 @@ export interface ExecuteOptions {
 
 // defaultExecutor can be replaced with an external service, etc
 export let defaultExecutor: CodeExecutor = {
-  async execute(source: string, options: ExecuteOptions): Promise<string> {
+  async execute(
+    source: string,
+    options: ExecuteOptions
+  ): Promise<string> {
     if (options.language !== "javascript") {
       return `Unsupported language: ${options.language}`;
     }
@@ -28,10 +30,8 @@ export let defaultExecutor: CodeExecutor = {
       return true;
     }
     return false;
-  }
-}
-
-
+  },
+};
 
 @component
 export class CodeBlock {
@@ -49,7 +49,18 @@ export class CodeBlock {
 
   handleIcon(collapsed: boolean = false): any {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="node-bullet">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="15"
+        height="15"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="node-bullet"
+      >
         <polyline points="16 18 22 12 16 6"></polyline>
         <polyline points="8 6 2 12 8 18"></polyline>
       </svg>
@@ -63,45 +74,52 @@ export class CodeBlock {
       when: (ctx: Context) => {
         if (!ctx.node) return false;
         if (ctx.node.raw.Rel === "Fields") return false;
-        if (ctx.node.parent && ctx.node.parent.hasComponent(Document)) return false;
+        if (ctx.node.parent && ctx.node.parent.hasComponent(Document))
+          return false;
         return true;
       },
       action: (ctx: Context) => {
         const com = new CodeBlock();
-        ctx.node.addComponent(com);
-        ctx.node.changed();
-        workbench.workspace.setExpanded(ctx.path.head, ctx.path.node, true);
-      }
+        if (ctx?.node) {
+          ctx.node.addComponent(com);
+          ctx.node.changed();
+          workbench.workspace.setExpanded(
+            ctx.path.head,
+            ctx.path.node,
+            true
+          );
+        }
+      },
     });
   }
-
 }
 
-
 const CodeEditor = {
-  oncreate({dom, attrs: {path}}) {
+  oncreate({ dom, attrs: { path } }) {
     const snippet = path.node.getComponent(CodeBlock);
+    //@ts-ignore
     dom.jarEditor = new window.CodeJar(dom, (editor) => {
       // highlight.js does not trim old tags,
       // let's do it by this hack.
       editor.textContent = editor.textContent;
+      //@ts-ignore
       window.hljs.highlightBlock(editor);
-      snippet.language = window.hljs.highlightAuto(editor.textContent).language || "";
+      snippet.language =
+        //@ts-ignore
+        window.hljs.highlightAuto(editor.textContent).language || "";
     });
     dom.jarEditor.updateCode(snippet.code);
-    dom.jarEditor.onUpdate(code => {
+    dom.jarEditor.onUpdate((code) => {
       snippet.code = code;
       path.node.changed();
     });
   },
 
-  view({attrs: {workbench, path}}) {
+  view({ attrs: { workbench, path } }) {
     // this cancels the keydown on the outline node
     // so you can use arrow keys normally
     const onkeydown = (e) => e.stopPropagation();
-    
-    return (
-      <div class="code-editor" onkeydown={onkeydown}></div>
-    )
-  }
-}
+
+    return <div class="code-editor" onkeydown={onkeydown}></div>;
+  },
+};
